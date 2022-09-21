15710001daa218349debe7f130d7e5fc753bae07
Mohammed Hafiz <65387738+Hafiz408@users.noreply.github.com>
2022-09-21 14:31:16 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,2 +1,2 @@
 hours = ['', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
-courses = ['', 'Production', 'Production(SW)', 'Automobile', 'Mechanical', 'Mechanical(SW)']
+courses = ['', 'Production', 'Production(SW)', 'Automobile', 'Mechanical', 'Mechanical(SW)', 'Data Science']
